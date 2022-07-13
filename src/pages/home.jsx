2e36715c55a0a9772bb033d86e43b92c7f971943
Ori Sason <orisason1@gmail.com>
2022-07-14 00:21:18 +0300

    added robot JSON to local storage

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,22 @@
+
+import { useEffect, useState } from 'react'
+import { roboService } from '../services/robo.service'
+
 export const Home = () => {
+
+    const [robots, setRobots] = useState(null)
+
+    useEffect(() => {
+        loadRobots()
+    }, [])
+
+    const loadRobots = async () => {
+        const robots = await roboService.query()
+        setRobots(robots)
+    }
+
+    if (!robots) return 'Loading'
+
     return <section className="home-page main-layout">
         <h1>APP</h1>
     </section>
