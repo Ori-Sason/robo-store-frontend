18c6de4e21fc6fbf81d2fecfb94f78dc8bac44f6
Ori Sason <orisason1@gmail.com>
2022-07-22 02:05:39 +0300

    added PWA files

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { HashRouter as Router } from 'react-router-dom'
 import configureStore from './configure-store';
 import { App } from './root-cmp';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './assets/scss/main.scss';
 
 const store = configureStore()
@@ -17,6 +18,11 @@ root.render(
     </Router>
   </Provider>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
