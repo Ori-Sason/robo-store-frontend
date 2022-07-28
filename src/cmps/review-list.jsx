a5d2ff0ab30e14e112d3fcffecaa1b5315109b21
Ori Sason <orisason1@gmail.com>
2022-07-28 11:51:10 +0300

    fixed review-list cmp reverse order

--- src/cmps/review-list.jsx
+++ src/cmps/review-list.jsx
@@ -1,7 +1,12 @@
 import { ReviewPreview } from './review-preview'
 
 export const ReviewList = ({ reviews, isShowWriter, isShowRobot }) => {
+    const reversedList = (list) => {
+        return [...list].sort((a, b) => (new Date(a.createdAt)).getTime() < (new Date(b.createdAt)).getTime() ? 1 : -1)
+    }
+    
     return <ol className="review-list" reversed>
-        {reviews.map(review => <ReviewPreview key={review._id} review={review} isShowWriter={isShowWriter} isShowRobot={isShowRobot} />)}
+        {reversedList(reviews).map(review => 
+            <ReviewPreview key={review._id} review={review} isShowWriter={isShowWriter} isShowRobot={isShowRobot} />)}
     </ol>
 }
